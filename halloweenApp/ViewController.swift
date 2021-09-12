19a331803a80a40659e2ae5f529f4161a83c5e63
Alvie <you@example.com>
2021-09-12 16:53:03 +0800

    Initial Commit

---
+++ halloweenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  halloweenApp
+//
+//  Created by Chen Arlays on 2021/9/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
